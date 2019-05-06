8d9dbd8176f29822ef3a378357802186cb735414
SawThanDar18 <sawthandar@ucsy.edu.mm>
2019-05-06 13:47:59 +0630

    operators_example

---
+++ src/operators.kt
@@ -0,0 +1,32 @@
+fun main(args : Array<String>){
+
+    //operators(+,-,*,%,/)
+    var num : Int
+    num = 10 + 5  // '+' can replace with '-,*,/,%'
+    println("Ans : " + "$num")
+
+    var num1 = 10
+    var num2 = 5
+    println(num1 + num2) // '+' can replace with '-,*,/,%'
+
+    //operators(increment, decrement)
+    var a = 10
+    ++a   // pre increment
+    println(a)
+
+    var b = 10
+    b++   // post increment
+    println(b)
+
+    var c = 10
+    --c   // pre decrement
+    println(c)
+
+    var d = 10
+    d--   // post decrement
+    println(d)
+
+    var e = 10
+    e += 10  // e = e +10 // '+' can replace with '-,*,/,%'
+    println(e)
+}
